1758708bc2ad02114097c6ee8ff3e5c5da3c5c75
alex g <alexgiurca9@gmail.com>
2023-01-02 13:35:20 -0500

    commit

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -22,5 +25,66 @@ public class Test {
         Print how many students are MathStudent with message -> "Math students = {numberOfMathStudents}"
         Print how many students are ScienceStudent with message -> "Science students = {numberOfScienceStudents}"
          */
+
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        while (students.size() < 3) {
+            UserQuestions.askToJoin();
+            String answer = scanner.nextLine();
+
+            if(answer.equalsIgnoreCase("n")) continue;
+
+            if (answer.equalsIgnoreCase("y")) {
+                UserQuestions.askFirstName();
+                String firstName = scanner.nextLine();
+
+                UserQuestions.askLastName();
+                String lastName = scanner.nextLine();
+
+                UserQuestions.askAge();
+                int age = scanner.nextInt();
+                try {
+                    Permission.checkAge(age);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                } finally {
+                    scanner.nextLine();
+                }
+
+                UserQuestions.askGender();
+                String gender = scanner.nextLine().toUpperCase();
+
+                UserQuestions.askClassName();
+                String className = scanner.next();
+                try {
+                    Permission.checkClassName(className);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    continue;
+                } finally {
+                    scanner.nextLine();
+                }
+
+                Student student = new Student(firstName, lastName, age, gender, className);
+                students.add(student);
+                System.out.println("Congratulations! You are registered for " + student.getClassName() + ".\n");
+            } else {
+                System.out.println("Invalid input! Please answer with \"Y\" or \"N\"");
+            }
+        }
+
+        System.out.println("\nRegistered students:");
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+
+        int numMathStudents = (int) students.stream().filter(s -> s.getClassName().equalsIgnoreCase("math")).count();
+        int numScienceStudents = (int) students.stream().filter(s -> s.getClassName().equalsIgnoreCase("science")).count();
+        System.out.println("\nMath students: " + numMathStudents);
+        System.out.println("Science students: " + numScienceStudents);
     }
-}
+    }
+
